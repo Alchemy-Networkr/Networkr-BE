de3d463b3f8bafb8a6eb39d38c9d3014ef5e76e7
bwapes <benwaples@gmail.com>
2020-10-01 16:00:05 -0700

    update test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,10 @@ const app = require('../lib/app');
 
 describe('networkr routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('should test my patience', () => {
+    expect(true).toEqual(true);
   });
 });

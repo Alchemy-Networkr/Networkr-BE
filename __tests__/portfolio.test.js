25049424c2faf88bec3b19537b8b07ba477f2d43
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-10-02 13:52:19 -0700

    finished findById method and testing

--- __tests__/portfolio.test.js
+++ __tests__/portfolio.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const PortfolioProject = require('../lib/models/portfolio-model');
 
 describe('networkr routes', () => {
   beforeEach(() => {
@@ -29,5 +30,22 @@ describe('networkr routes', () => {
         collaborators: ['ben, edgar, adrian'], 
         open: true
       }));
+  });
+
+  it('should find portfolio_project by ID via GET', async() => {
+    const addProject = await PortfolioProject.insert({ 
+      title: 'title', 
+      primaryLanguage: 'js', 
+      date: '2020-10-02', 
+      githubLink: 'google.com', 
+      description: 'this is my project', 
+      collaborators: ['ben, edgar, adrian'], 
+      open: true });
+
+
+
+    return await request(app)
+      .get(`/api/v1/portfolioProjects/${addProject.portfolioId}`)
+      .then(res => expect(res.body).toEqual({ ...addProject, date: expect.stringContaining('2020-10-02') }));
   });
 });

e25e7aa19c6a059c4540bae3be0e27c397ff1465
Adrian Simental <asimental100@hotmail.com>
2020-10-02 12:00:42 -0700

    working on insert

--- __tests__/portfolio.test.js
+++ __tests__/portfolio.test.js
@@ -8,7 +8,26 @@ describe('networkr routes', () => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
 
-  it('should test my patience', () => {
-    expect(true).toEqual(true);
+  it('should test POST route', async() => {
+    return await request(app)
+      .post('/api/v1/portfolioProjects')
+      .send({ 
+        title: 'title', 
+        primaryLanguage: 'js', 
+        date: '2020-10-02', 
+        githubLink: 'google.com', 
+        description: 'this is my project', 
+        collaborators: ['ben, edgar, adrian'], 
+        open: true })
+      .then(res => expect(res.body).toEqual({ 
+        portfolioId: expect.any(String),
+        title: 'title', 
+        primaryLanguage: 'js', 
+        date: expect.stringContaining('2020-10-02'), 
+        githubLink: 'google.com', 
+        description: 'this is my project', 
+        collaborators: ['ben, edgar, adrian'], 
+        open: true
+      }));
   });
 });
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 
-const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console

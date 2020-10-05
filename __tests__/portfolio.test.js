4ad052af8d1a82d4e4b88520fe53f2f1a675db68
bwapes <benwaples@gmail.com>
2020-10-05 10:32:57 -0700

    comment delete and update

--- __tests__/portfolio.test.js
+++ __tests__/portfolio.test.js
@@ -2,6 +2,7 @@ require('../lib/data/data-helper');
 const request = require('supertest');
 const app = require('../lib/app');
 const PortfolioProject = require('../lib/models/portfolio-model');
+const PortfolioComment = require('../lib/models/portfolio-comment');
 
 describe('networkr routes', () => {
 
@@ -93,5 +94,27 @@ describe('PortfolioComment class', () => {
       .post('/api/v1/portfolioComments')
       .send(addComment)
       .then(res => expect(res.body).toEqual({ ...addComment, id: expect.any(String), portfolioId: expect.any(String) }));
+  });
+
+  it('should return all comments via GET', async() => {
+    return await request(app)
+      .get('/api/v1/portfolioComments')
+      .then(res => expect(res.body.length).toEqual(50));
+  });
+  // add routes for auth -> one for a good request, one for a bad request(user tries to delete a comment they didnt make)
+  it('should delete a comment via DELETE', async() => {
+    const firstComment = (await PortfolioComment.findAll())[0];
+    return await request(app)
+      .delete(`/api/v1/portfolioComments/${firstComment.id}`)
+      .then(res => expect(res.body).toEqual(firstComment));
+  });
+
+  it('should update a comment via PATCH', async() => {
+    const firstComment = (await PortfolioComment.findAll())[0];
+    const updatedComment = { comment: 'test the comment update route' };
+    return await request(app)
+      .patch(`/api/v1/portfolioComments/${firstComment.id}`)
+      .send(updatedComment)
+      .then(res => expect(res.body).toEqual({ ...firstComment, comment: updatedComment.comment }));
   });
 });

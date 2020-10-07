3302ac47340e6305657058822f31e7f74da7c14d
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-10-06 18:00:47 -0700

    fixed pool.query to add owner_id

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/portfolioProjects":["static\u002Fchunks\u002Fpages\u002FportfolioProjects.js"],sortedPages:["\u002F_app","\u002F_error","\u002FportfolioProjects"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
